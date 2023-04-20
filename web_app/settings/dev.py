797f03f1aa288f85ec6b514be375f78b60dde005
Mickaël <miket972@gmail.com>
2023-04-21 00:49:22 +0200

    set database settings

--- web_app/settings/dev.py
+++ web_app/settings/dev.py
@@ -3,18 +3,6 @@ from . import *  # noqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
-        "OPTIONS": {"sslmode": "allow"},
-    }
-}
-
 # CACHES = {
 #     "default": {
 #         "BACKEND": "django.core.cache.backends.dummy.DummyCache",
